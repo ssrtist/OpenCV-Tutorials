3b99beea534167686414cd9d1859da595ea42a49
ssrtist <ssrtist@yahoo.com>
2022-12-05 12:11:25 -0500

    tutorial3 #2

--- tutorial3.py
+++ tutorial3.py
@@ -9,9 +9,9 @@ while True:
     height = int(frame.shape[0])
     smaller_frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
     image = np.zeros(frame.shape, np.uint8)
-    image[:height//2, :width//2] = smaller_frame
+    image[:height//2, :width//2] = cv2.rotate(smaller_frame, cv2.cv2.ROTATE_180)
     image[height//2:, :width//2] = smaller_frame
-    image[:height//2, width//2:] = smaller_frame
+    image[:height//2, width//2:] = cv2.rotate(smaller_frame, cv2.cv2.ROTATE_180)
     image[height//2:, width//2:] = smaller_frame
     cv2.imshow('image', image)
     cv2.imshow('frame', frame)
